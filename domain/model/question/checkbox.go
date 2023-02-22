2fab77cc198b129bad0d9f50adce2e9d00964476
Shion Ichikawa <shion1305@gmail.com>
2023-02-22 15:12:17 +0900

    🐛 (model/question)change casting of OptionsData, OptionsOrder

--- domain/model/question/checkbox.go
+++ domain/model/question/checkbox.go
@@ -40,16 +40,20 @@ func NewCheckBoxQuestion(
 }
 
 func ImportCheckBoxQuestion(q StandardQuestion) (*CheckBoxQuestion, error) {
-	// check if customs has CheckBoxOptionsField as map[int64]string, return error if not
+	// CheckBoxOptionsField should be map[string]string
+
+	// Although you cannot cast map[string]interface{} to map[string]string,
+	// you have to iterate over the map and cast each value to string.
+	// First, check if customs has CheckBoxOptionsField as map[string]interface{}, return error if not.
 	optionsDataI, has := q.Customs[CheckBoxOptionsField]
 	if !has {
 		return nil, errors.New(
 			fmt.Sprintf("\"%s\" is required for CheckBoxQuestion", CheckBoxOptionsField))
 	}
-	optionsData, ok := optionsDataI.(map[int64]string)
+	optionsData, ok := optionsDataI.(map[string]interface{})
 	if !ok {
 		return nil, errors.New(
-			fmt.Sprintf("\"%s\" must be map[int64]string for CheckBoxQuestion", CheckBoxOptionsField))
+			fmt.Sprintf("\"%s\" must be map[string]string for CheckBoxQuestion", CheckBoxOptionsField))
 	}
 
 	// check if customs has "order" as []int64, return error if not
@@ -58,7 +62,7 @@ func ImportCheckBoxQuestion(q StandardQuestion) (*CheckBoxQuestion, error) {
 		return nil, errors.New(
 			fmt.Sprintf("\"%s\" is required for CheckBoxQuestion", CheckBoxOptionsOrderField))
 	}
-	optionsOrderData, ok := optionsOrderDataI.([]int64)
+	optionsOrderData, ok := optionsOrderDataI.([]interface{})
 	if !ok {
 		return nil, errors.New(
 			fmt.Sprintf("\"%s\" must be []int64 for CheckBoxQuestion", CheckBoxOptionsOrderField))
@@ -67,12 +71,27 @@ func ImportCheckBoxQuestion(q StandardQuestion) (*CheckBoxQuestion, error) {
 	options := make([]CheckBoxOption, 0, len(optionsData))
 	optionsOrder := make([]CheckBoxOptionID, 0, len(optionsOrderData))
 	for _, id := range optionsOrderData {
-		optionsOrder = append(optionsOrder, identity.NewID(id))
+		i, ok := id.(int64)
+		if !ok {
+			return nil, errors.New(
+				fmt.Sprintf("Option order must be int64 for CheckBoxQuestion"))
+		}
+		optionsOrder = append(optionsOrder, identity.NewID(i))
 	}
 
-	for id, text := range optionsData {
+	for id, textI := range optionsData {
+		// here we cast textI to string
+		text, ok := textI.(string)
+		if !ok {
+			return nil, errors.New(
+				fmt.Sprintf("Option text must be string for CheckBoxQuestion"))
+		}
+		i, err := identity.ImportID(id)
+		if err != nil {
+			return nil, err
+		}
 		options = append(options, CheckBoxOption{
-			ID:   identity.NewID(id),
+			ID:   i,
 			Text: text,
 		})
 	}
@@ -81,10 +100,15 @@ func ImportCheckBoxQuestion(q StandardQuestion) (*CheckBoxQuestion, error) {
 
 func (q CheckBoxQuestion) Export() StandardQuestion {
 	customs := make(map[string]interface{})
-	options := make(map[int64]string, len(q.Options))
+	options := make(map[string]string, len(q.Options))
 	for _, option := range q.Options {
-		options[option.ID.GetValue()] = option.Text
+		options[option.ID.ExportID()] = option.Text
+	}
+	optionsOrder := make([]int64, 0, len(q.OptionsOrder))
+	for _, id := range q.OptionsOrder {
+		optionsOrder = append(optionsOrder, id.GetValue())
 	}
 	customs[CheckBoxOptionsField] = options
+	customs[CheckBoxOptionsOrderField] = optionsOrder
 	return NewStandardQuestion(TypeCheckBox, q.ID, q.Text, customs)
 }
